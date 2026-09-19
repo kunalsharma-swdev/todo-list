0db9e2ede85603c6905e2016a8b752ea5e52461f
Kunal Sharma <kunalsharma.swdev@gmail.com>
2026-09-19 13:41:04 +0530

    completed prototype ui of todo form

---
+++ src/todo.js
@@ -0,0 +1,15 @@
+let todos = [];
+function todo(title1, description1, duedate1,priority1, notes1,projects1){
+    let today = new Date();
+    return {
+        title: title1,
+        description: description1,
+        duedate: duedate1,
+        priority: priority1,
+        notes: notes1,
+        checklist: false,
+        project: projects1,
+        dateadded: today.toLocaleDateString(),
+        datecompleted: null
+    };
+}
